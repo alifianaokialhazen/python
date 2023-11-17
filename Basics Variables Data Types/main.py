ffd3e5cc9fb16fc3f771eb801223b6b419cfc3f8
alifian <alifian.aoki@student.alhazen.academy>
2023-11-17 09:28:38 +0700

    add files

---
+++ Basics Variables Data Types/main.py
@@ -0,0 +1,24 @@
+list_data = [1,2,3,4,5]
+
+tuple_darta = (1,2,3,4,5)
+
+set_data = {1,2,3,4,5}
+
+dictionary_data = {
+    'one' : 1,
+    'two' : 2,
+    'three' : 3,
+    'four' : 4,
+    'five' : 5,
+}
+print('start')
+number_list = list(range(12))
+print(number_list)
+
+print('start,stop')
+number_list1 = list(range(5,12))
+print(number_list1)
+
+print('start,stop,step')
+number_list2 = list(range(5,12,3))
+print(number_list2)
